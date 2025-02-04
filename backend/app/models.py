675aa5f111bd1fcf8683e8957bdb5df247e8d8f8
Stephan RAHARISON Hery Miharintsoa <stephan@constellation-group.local>
2025-02-04 17:50:39 +0300

    Select prospect

--- backend/app/models.py
+++ backend/app/models.py
@@ -41,5 +41,43 @@ class Prospect(db.Model):
     headquarters_city = db.Column(db.String(255))
     address = db.Column(db.Text)
 
-def __repr__(self):
-    return f"< Prospect {self.first_name} {self.last_name}>"
+    def __repr__(self):
+        return f"<Prospect {self.first_name} {self.last_name}>"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "civility": self.civility,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "company_linkedin": self.company_linkedin,
+            "linkedin_url": self.linkedin_url,
+            "intent_signal": self.intent_signal,
+            "other_1": self.other_1,
+            "other_2": self.other_2,
+            "other_3": self.other_3,
+            "company": self.company,
+            "title": self.title,
+            "email": self.email,
+            "status": self.status,
+            "priority": self.priority,
+            "mobile_phone": self.mobile_phone,
+            "direct_line": self.direct_line,
+            "switchboard": self.switchboard,
+            "other_phone_1": self.other_phone_1,
+            "other_phone_2": self.other_phone_2,
+            "job_description": self.job_description,
+            "company_website": self.company_website,
+            "years_in_position": self.years_in_position,
+            "years_in_company": self.years_in_company,
+            "industry": self.industry,
+            "ca": self.ca,
+            "company_employee_count": self.company_employee_count,
+            "company_employee_range": self.company_employee_range,
+            "company_year_founded": self.company_year_founded,
+            "company_description": self.company_description,
+            "vat": self.vat,
+            "headquarters_pc": self.headquarters_pc,
+            "headquarters_city": self.headquarters_city,
+            "address": self.address
+        }
